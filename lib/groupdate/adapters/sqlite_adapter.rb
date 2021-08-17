7a78906f0df37d28ad241616c66cb3b3fe45ab5f
Andrew Kane <andrew@ankane.org>
2021-08-16 19:46:41 -0700

    Added SQLite function [skip ci]

--- lib/groupdate/adapters/sqlite_adapter.rb
+++ lib/groupdate/adapters/sqlite_adapter.rb
@@ -2,7 +2,6 @@ module Groupdate
   module Adapters
     class SQLiteAdapter < BaseAdapter
       def group_clause
-        raise Groupdate::Error, "Time zones not supported for SQLite" unless @time_zone.utc_offset.zero?
         raise Groupdate::Error, "day_start not supported for SQLite" unless day_start.zero?
 
         query =
@@ -36,13 +35,28 @@ module Groupdate
               when :month
                 "%Y-%m-01 00:00:00 UTC"
               when :quarter
-                raise Groupdate::Error, "Quarter not supported for SQLite"
+                nil
               else # year
                 "%Y-01-01 00:00:00 UTC"
               end
 
             ["strftime(?, #{column})", format]
           end
+
+        if !@time_zone.utc_offset.zero? || period == :quarter
+          utc = ActiveSupport::TimeZone["UTC"]
+          db = @relation.connection.raw_connection
+          db.create_function("groupdate", 3) do |func, value, period, time_zone|
+            if value.nil?
+              func.result = nil
+            else
+              result = [value].group_by_period(period, time_zone: time_zone, dates: false) { utc.parse(value) }.keys[0]
+              result = result.in_time_zone(utc).strftime("%Y-%m-%d %H:%M:%S") if result.is_a?(Time)
+              func.result = result
+            end
+          end
+          query = ["groupdate(#{column}, ?, ?)", period, @time_zone.tzinfo.name]
+        end
 
         @relation.send(:sanitize_sql_array, query)
       end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -89,10 +89,6 @@ class Minitest::Test
   def call_method(method, field, options)
     if enumerable?
       Hash[@users.group_by_period(method, **options) { |u| u.send(field) }.map { |k, v| [k, v.size] }]
-    elsif sqlite? && (method == :quarter || options[:time_zone] || options[:day_start] || (Time.zone && options[:time_zone] != false))
-      error = assert_raises(Groupdate::Error) { User.group_by_period(method, field, **options).count }
-      assert_includes error.message, "not supported for SQLite"
-      skip
     else
       User.group_by_period(method, field, **options).count
     end
